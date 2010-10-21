9c774f705bb047b929b6608f856cdaf5acc60e77
Piotr Solnica <piotr.solnica@gmail.com>
2010-10-21 15:15:59 +0200

    Specs for parent errors

--- spec/public/validations_ext_spec.rb
+++ spec/public/validations_ext_spec.rb
@@ -7,9 +7,19 @@ describe DataMapper::ValidationsExt do
         include DataMapper::Resource
 
         property :id, Serial
-        property :name, String
+        property :name, String, :required => true
 
+        belongs_to :group, :required => false
         has n, :roles
+      end
+
+      class Group
+        include DataMapper::Resource
+
+        property :id, Serial
+        property :name, String, :length => 6..255
+
+        has n, :users
       end
 
       class Role
@@ -23,47 +33,91 @@ describe DataMapper::ValidationsExt do
       DataMapper.auto_migrate!
     end
 
-    describe "when children are not valid" do
-      before :all do
-        @user = User.create(:name => 'John')
+    describe "Parent errors" do
+      describe "when the parent is valid" do
+        before :all do
+          @group = Group.new(:name => "DataMapperists")
+          @user  = User.new(:name => "John", :group => @group)
+        end
 
-        @role = Role.new
-        @user.roles << @role
-        @role_errors = [ @role.errors ]
+        describe "#save" do
+          subject { @user.save }
+          it { should be(true) }
+        end
+
+        describe "#errors" do
+          before { @user.save }
+          subject { @user.errors[:group] }
+          it { should be_blank }
+        end
       end
 
-      describe "#save" do
-        subject { @user.save }
+      describe "when the parent is valid" do
+        before :all do
+          @group = Group.new(:name => "a")
+          @user  = User.new(:name => "John", :group => @group)
+        end
 
-        it { should be(false) }
-      end
+        describe "#save" do
+          subject { @user.save }
+          it { should be(false) }
+        end
 
-      describe "#errors" do
-        before  { @user.save }
-        subject { @user.errors[:roles] }
+        describe "#errors" do
+          before { @user.save }
+          let(:expected_errors) { @group.errors }
+          subject { @user.errors[:group] }
 
-        it { should_not be_nil }
-        it { should be_kind_of(Array) }
-        it { should eql(@role_errors) }
+          it { should_not be_nil }
+          it { should be_kind_of(DataMapper::Validations::ValidationErrors) }
+          it { should eql(expected_errors) }
+        end
       end
     end
 
-    describe "when children are valid" do
-      before :all do
-        @user = User.create(:name => 'Jane')
-        @user.roles << @user.roles.new(:name => "SuperUser")
+    describe "Child errors" do
+      describe "when children are not valid" do
+        before :all do
+          @user = User.create(:name => 'John')
+
+          @role = Role.new
+          @user.roles << @role
+          @role_errors = [ @role.errors ]
+        end
+
+        describe "#save" do
+          subject { @user.save }
+
+          it { should be(false) }
+        end
+
+        describe "#errors" do
+          before  { @user.save }
+          subject { @user.errors[:roles] }
+
+          it { should_not be_nil }
+          it { should be_kind_of(Array) }
+          it { should eql(@role_errors) }
+        end
       end
 
-      describe "#save" do
-        subject { @user.save }
+      describe "when children are valid" do
+        before :all do
+          @user = User.create(:name => 'Jane')
+          @user.roles << @user.roles.new(:name => "SuperUser")
+        end
 
-        it { should be(true) }
-      end
+        describe "#save" do
+          subject { @user.save }
 
-      describe "#errors" do
-        subject { @user.errors[:roles] }
+          it { should be(true) }
+        end
 
-        it { should be_blank }
+        describe "#errors" do
+          subject { @user.errors[:roles] }
+
+          it { should be_blank }
+        end
       end
     end
   end
